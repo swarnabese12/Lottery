57cd65b30f5712ba16a2cda6778e792dbc6c63de
swarnabese12 <lakshmibese@gmail.com>
2025-01-22 14:27:40 +0530

    Updated lottery smart contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
 "use client";
 import { useWallet } from "@solana/wallet-adapter-react";
 import Image from "next/image";
-import { FaCheckCircle } from "react-icons/fa"; // Import the check circle icon
+import { FaCheckCircle } from "react-icons/fa";
 import HomeComponent from "./components/HomeComponent";
+import { WalletButtonImport } from "./components/WalletContextProvider";
 
 export default function Home() {
   const { publicKey } = useWallet();
 
   return (
     <main className="min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 text-gray-100 flex">
-      {/* Left Side: Image */}
       <div className="flex-1 flex items-center justify-center">
         <div className="rounded-lg overflow-hidden shadow-md">
           <Image
             src="/images/lot-pic-2.png"
             alt="Lottery"
-            width={700} // Adjusted width for the image
-            height={700} // Adjusted height for the image
-            layout="intrinsic" // Use intrinsic to maintain image ratio
+            width={700}
+            height={700}
+            layout="intrinsic"
           />
         </div>
       </div>
 
-      {/* Right Side: Text and Components */}
       <div className="flex-1 flex flex-col items-center justify-center p-8 gap-6 rounded-lg">
 
         <div className="flex flex-col gap-4 items-center text-center">
@@ -32,9 +31,7 @@ export default function Home() {
               <FaCheckCircle className="ml-2 text-green-500 text-xl rounded-full" />
             </h2>
           ) : (
-            <h2 className="text-xl font-semibold text-red-600">
-              Please connect your wallet to continue
-            </h2>
+            <WalletButtonImport />
           )}
         </div>
 
